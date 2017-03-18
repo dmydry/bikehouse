1af17119586273bead7b0caf72d5fddb9f6100b2
dmydry <dmitriy.gurinenko@gmail.com>
2017-03-18 22:26:02 +0800

    header-menu added

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="header-menu">
+    <b-navbar toggleable type="inverse" variant="inverse">
+
+      <b-nav-toggle target="nav_collapse"/>
+
+      <a class="navbar-brand" style="color:white" to="/">
+        <span>{{title}}</span>
+      </a>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-nav is-nav-bar>
+          <b-nav-item>{{menu.support}}</b-nav-item>
+          <b-nav-item>{{menu.docs}}</b-nav-item>
+          <b-nav-item>{{menu.contact}}</b-nav-item>
+        </b-nav>
+        <b-nav is-nav-bar class="ml-auto">
+          <b-nav-item-dropdown :text="menu.lang" right-alignment>
+            <a href class="dropdown-item active">
+              EN
+            </a>
+            <a href class="dropdown-item">
+              ES
+            </a>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown right-alignment>
+            <template slot="text">
+              <span style="font-weight: bold;">{{menu.user}}</span>
+            </template>
+            <a href class="dropdown-item">
+              Profile
+            </a>
+            <a href class="dropdown-item">
+              Sign out
+            </a>
+          </b-nav-item-dropdown>
+        </b-nav>
+      </b-collapse>
+
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header-menu',
+    data() {
+      return {
+        title: 'BikeHouse',
+        menu: {
+          support: 'Support',
+          docs: 'Docs',
+          contact: 'Contact Us',
+          lang: 'Lang',
+          user: 'User',
+        },
+      };
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../../node_modules/bootstrap-vue/node_modules/bootstrap/dist/css/bootstrap.css';
+</style>

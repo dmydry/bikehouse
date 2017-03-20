5bb2f5ff005ca61bf6ea10b73c7b841056fd4187
dmydry <dmitriy.gurinenko@gmail.com>
2017-03-20 22:42:39 +0800

    fixed navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,18 +4,16 @@
 
       <b-nav-toggle target="nav_collapse"/>
 
-      <a class="navbar-brand" style="color:white" to="/">
-        <span><router-link to="/" class="navbar-brand">{{title}}</router-link></span>
-      </a>
+      <b-link class="navbar-brand" to="/">
+        <span>{{title}}</span>
+      </b-link>
 
       <b-collapse is-nav id="nav_collapse">
         <b-nav is-nav-bar>
-          <b-nav-item><router-link to="support" class="nav-link">{{menu.support}}</router-link></b-nav-item>
-          <b-nav-item><router-link to="docs" class="nav-link">{{menu.docs}}</router-link></b-nav-item>
-          <b-nav-item>
-            <a v-if="$route.path === '/'" href="#contact" class="nav-link">{{menu.contact}}</a>
-            <a v-else href="/#contact" class="nav-link">{{menu.contact}}</a>
-          </b-nav-item>
+          <b-nav-item href='/support' class="nav-link">{{menu.support}}</b-nav-item>
+          <b-nav-item href='/docs' class="nav-link">{{menu.docs}}</b-nav-item>
+          <b-nav-item href='/#contact' class="nav-link">Contact Us</b-nav-item>
+
         </b-nav>
         <b-nav is-nav-bar class="ml-auto">
           <b-nav-item-dropdown :text="menu.lang" right-alignment class="nav-link">
